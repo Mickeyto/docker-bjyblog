be3e4a07aa2f70dbc0efa0a2e08496c7b4afa8ca
mickeyto <wangmin012680@gmail.com>
2021-01-26 22:01:26 +0800

    fix

--- web/blog/resources/views/layouts/home.blade.php
+++ web/blog/resources/views/layouts/home.blade.php
@@ -177,7 +177,7 @@
                     <dd>{{ __('Contact Email') }}：<a href="mailto:{!! config('bjyblog.admin_email') !!}">{!! config('bjyblog.admin_email') !!}</a></dd>
                 @endif
                 @if(!empty(config('bjyblog.icp')) && config('app.locale') === 'zh-CN')
-                    <dd>{{ __('ICP') }}：{{ config('bjyblog.icp') }}</dd>
+                    <dd><a href="https://beian.miit.gov.cn/" target="_blank">{{ __('ICP') }}：{{ config('bjyblog.icp') }}</a></dd>
                 @endif
             </dl>
 
